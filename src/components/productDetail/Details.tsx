f993bdca92d4d63ba7b374f39445b0bd0137ab4b
guluehmedova <guliehmedova19@gmail.com>
2024-01-20 14:25:20 +0400

    detail half-star

--- src/components/productDetail/Details.tsx
+++ src/components/productDetail/Details.tsx
@@ -29,7 +29,57 @@ const Details = () => {
   const product: ProductTypes = useSelector((state: RootState) => state.productDetail.product);
   const loading = useSelector((state: RootState) => state.productDetail.loading);
   const totalReviewCount = useSelector((state: RootState) => state.review.totalReviewCount);
+
   const reviewRating = useSelector((state: RootState) => state.review.reviewRating);
+  const totalStars = 5;
+
+  const firstStarMethod = () => {
+    if (reviewRating) {
+      return [...Array(totalStars)].map((_, i) =>
+        i + 1 > reviewRating ? (
+          <svg xmlns="http://www.w3.org/2000/svg" key={i} className="text-yellow-500 w-5 h-auto fill-current"
+            viewBox="0 0 16 16">
+            <path
+              d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" key={i} className="text-yellow-500 w-5 h-auto fill-current"
+            viewBox="0 0 16 16">
+            <path
+              d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+          </svg>
+        )
+      )
+    }
+  };
+
+  const secondStarMethod = () => {
+    return [...Array(totalStars)].map((_, i) =>
+      // check if current star should be half
+      i < reviewRating && i + 1 > reviewRating ? (
+        <svg xmlns="http://www.w3.org/2000/svg" key={i} className="text-yellow-500 w-5 h-auto fill-current"
+          viewBox="0 0 16 16">
+          <path
+            d="M5.354 5.119 7.538.792A.516.516 0 0 1 8 .5c.183 0 .366.097.465.292l2.184 4.327 4.898.696A.537.537 0 0 1 16 6.32a.548.548 0 0 1-.17.445l-3.523 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256a.52.52 0 0 1-.146.05c-.342.06-.668-.254-.6-.642l.83-4.73L.173 6.765a.55.55 0 0 1-.172-.403.58.58 0 0 1 .085-.302.513.513 0 0 1 .37-.245l4.898-.696zM8 12.027a.5.5 0 0 1 .232.056l3.686 1.894-.694-3.957a.565.565 0 0 1 .162-.505l2.907-2.77-4.052-.576a.525.525 0 0 1-.393-.288L8.001 2.223 8 2.226v9.8z" />
+        </svg>
+
+      ) : // check if current star should be full
+        i < reviewRating ? (
+          <svg xmlns="http://www.w3.org/2000/svg" key={i} className="text-yellow-500 w-5 h-auto fill-current"
+            viewBox="0 0 16 16">
+            <path
+              d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
+          </svg>
+        ) : (
+          // else, current star should be empty
+          <svg xmlns="http://www.w3.org/2000/svg" key={i} className="text-yellow-500 w-5 h-auto fill-current"
+            viewBox="0 0 16 16">
+            <path
+              d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+          </svg>
+        )
+    );
+  };
 
   const userId_Int = useMemo(() => {
     if (userId) {
@@ -42,12 +92,6 @@ const Details = () => {
       return parseInt(productId);
     }
   }, [productId]);
-
-  const reviewCell = useMemo(() => {
-    if (reviewRating) {
-      return Math.ceil(reviewRating);
-    }
-  }, [reviewRating]);
 
   useEffect(() => {
     if (productID_Int) {
@@ -188,9 +232,7 @@ const Details = () => {
                 </div>
                 <div className="flex gap-[18px] mt-4 mb-5 items-center">
                   <div className="flex items-center gap-1">
-                    {[...Array(reviewCell)].map((_, index) => (
-                      <span className="cursor-pointer block text-2xl" key={index} style={{ color: '#ffc107' }}>&#9733;</span>
-                    ))}
+                    {Number.isInteger(reviewRating) ? firstStarMethod() : secondStarMethod()}
                   </div>
                   <span className="text-[#9F9F9F] text-[13px] border-l border-[#9F9F9F] block pl-[22px]">{totalReviewCount && totalReviewCount} Customer Review</span>
                 </div>
